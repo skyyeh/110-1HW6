b45dc3f396aa2b0f53dee486042f49ed79606f40
于翔 林 <108111120@mail.aeust.edu.tw>
2021-12-08 15:03:07 +0800

    HW6

--- 110-1HW6/Reg.aspx.cs
+++ 110-1HW6/Reg.aspx.cs
@@ -5,10 +5,18 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace _110_1HW6 {
-    public partial class Reg : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
+namespace _110_1HW6
+{
+    public partial class Reg : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
 
+        }
+
+        protected void btn_Submit_Click(object sender, EventArgs e)
+        {
+            lb_Msg.Text = tb_id.Text + "合法";
         }
     }
 }
